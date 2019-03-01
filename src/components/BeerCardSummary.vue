e39943893f911c7601b788a2c2106e1235af94fc
Ryu Komiyama <Ryu.Komiyama@bcbsnc.com>
2019-03-01 01:58:01 -0500

    Makes data table results expandable and contains details on selected row

---
+++ src/components/BeerCardSummary.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <v-card-title>
+      <h1>
+        {{ beer.name }}
+        <span class="tagline font-italic">{{ beer.tagline }}</span>
+      </h1>
+    </v-card-title>
+    <v-card-text class="text-xs-left pt-0">
+      <v-layout>
+        <v-flex xs6>
+          <BeerCardValues :beer="beer"/>
+        </v-flex>
+        <v-flex xs6>
+          <div class="font-weight-bold">Description</div>
+          <div>{{ beer.description }}</div>
+          <div class="font-weight-bold">Brewers tips</div>
+          <div>{{ beer.brewers_tips }}</div>
+          <div class="font-weight-bold">Food pairing</div>
+          <div v-for="(food, i) in beer.food_pairing" :key="i">{{ food }}</div>
+        </v-flex>
+      </v-layout>
+      <v-slide-y-transition>
+        <div v-show="showMore">
+          <v-divider></v-divider>
+          <BeerCardDetails :ingredients="beer.ingredients" :method="beer.method"/>
+        </div>
+      </v-slide-y-transition>
+      <v-flex text-xs-center>
+        <v-btn flat @click="showMore = !showMore">{{ showMore ? 'Show Less' : 'Show More' }}</v-btn>
+      </v-flex>
+    </v-card-text>
+  </div>
+</template>
+
+<script>
+import BeerCardValues from "./BeerCardValues";
+import BeerCardDetails from "./BeerCardDetails";
+
+export default {
+  name: "BeerCardSummary",
+  data() {
+    return {
+      showMore: false
+    };
+  },
+  props: {
+    beer: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  components: {
+    BeerCardValues,
+    BeerCardDetails
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.tagline {
+  color: grey;
+}
+</style>
---
+++ src/components/BeerCardValues.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-layout>
+    <v-flex xs6>
+      <h3>ABV</h3>
+      <h3>IBU</h3>
+      <h3>EBC</h3>
+      <h3>SRM</h3>
+      <div>First brewed</div>
+      <div>Volume</div>
+      <div>Attenuation level</div>
+      <div>pH</div>
+      <div>Boil volume</div>
+      <div>Target OG</div>
+      <div>Target FG</div>
+      <div>Contributed by</div>
+    </v-flex>
+    <v-flex xs6>
+      <h3>{{ beer.abv }}%</h3>
+      <h3>{{ beer.ibu }} BU</h3>
+      <h3>{{ beer.ebc }}</h3>
+      <h3>{{ beer.srm }}</h3>
+      <div>{{ beer.first_brewed }}</div>
+      <div>{{ beer.volume.value }} {{ beer.volume.unit }}</div>
+      <div>{{ beer.attenuation_level }}</div>
+      <div>{{ beer.ph }}</div>
+      <div>{{ beer.boil_volume.value }} {{ beer.boil_volume.unit }}</div>
+      <div>{{ beer.target_og }}</div>
+      <div>{{ beer.target_fg }}</div>
+      <div>{{ beer.contributed_by }}</div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "BeerCardValues",
+  props: {
+    beer: {
+      type: Object,
+      default: () => {}
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
